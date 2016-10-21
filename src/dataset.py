4e832ab17928ab60bed2acb3bc9b1a09b756ac62
andrew moyes <amoyes01@qub.ac.uk>
2016-10-21 16:37:16 +0000

    before yeild

--- src/dataset.py
+++ src/dataset.py
@@ -5,6 +5,7 @@ from os import walk, path
 import argparse
 import scipy.io
 import cv2
+from sklearn.feature_extraction import image as skimage
 
 # module accepts a root directory of the dataset and loops through each folder, extracting image data and image labels
 # assumes that each image is in its own folder (as in the CRCHistoPhentotypes dataset)
@@ -14,10 +15,14 @@ class dataset:
         self.root_directory = root_directory
         self.window_size = window_size
 
-    def build(self):
-        dirs = [x for x in walk(self.root_directory)]
-        print("[INFO] got " + str(len(dirs)) + " images")
-        for root, dirs, files in dirs:
+    def build(self, showImages=False):
+        walk_result = [x for x in walk(self.root_directory)]
+        print("[INFO] got " + str(len(walk_result)) + " images")
+
+        image_patches = []
+        label_patches = []
+
+        for root, dirs, files in walk_result:
             if len(files) == 2:
                 # is valid
                 if files[1].split(".")[-1] == "mat":
@@ -29,19 +34,28 @@ class dataset:
 
                 print("[INFO] reading " + label_path)
                 mat = scipy.io.loadmat(label_path)
-                mitosis_pixels = np.array(mat["detection"]).astype(np.uint8)
-                print(mitosis_pixels[:, 0])
+                mitosis_pixels = np.array(mat["detection"]).astype(np.int) - 1
                 # we now have all pixels in the image which are part of a mitosis
                 # next we need to create a window for each pixel in the image and assign a class to it
                 img = Image.open(image_path)
-                img_array = np.array(img)
-                img_array[mitosis_pixels[:, 0]*2, mitosis_pixels[:, 1]*2, :] = (0, 0, 0)
-                cv2.imshow("mitosis pixels", img_array)
-                cv2.waitKey(0)
+                img_array = np.array(img) # actual image as (500,500,3)
+                label_array = np.zeros(img_array.shape)
+                label_array[mitosis_pixels[:, 1], mitosis_pixels[:, 0], :] = (1, 1, 1)
 
+                if showImages == True:
+                    cv2.imshow("mitosis pixels", np.hstack((img_array/255, label_array)))
+                    cv2.waitKey(0)
 
+                current_img_patches = skimage.extract_patches_2d(img_array, (self.window_size, self.window_size))
+                current_label_patches = skimage.extract_patches_2d(label_array, (self.window_size, self.window_size))
+                print(current_img_patches.shape)
+                #image_patches.append(current_img_patches)
+                #label_patches.append(current_label_patches)
 
-
+        np_image_patches = np.array(image_patches)
+        np_label_patches = np.array(label_patches)
+        print("[INFO] np_image_patches shape " + str(np_image_patches.shape))
+        print("[INFO] np_label_patches shape " + str(np_label_patches.shape))
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-r", "--root", type=str, default='', help="(required) root directory of dataset")
